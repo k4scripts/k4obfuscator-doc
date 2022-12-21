3aa2dc7aa6006511b0a0174a81a795f65ad1fa4e
uniquadev <uniquadev@gmail.com>
2022-12-21 18:30:55 +0100

    initial commit

---
+++ docs/intro.md
@@ -0,0 +1,41 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro
+
+Let's discover **K4Obfuscator in less than 5 minutes**.
+
+## Getting Started
+
+Get started by **requesting an obfuscation key to K4Obfuscator bot**.
+
+### What you'll need
+
+- [Lua Script](https://www.lua.org/manual/5.1/):
+  - You must follow Lua 5.1 syntax, as VM generation is developed around it.
+
+## Obfuscate your first script
+
+Let's prepare a script to obfuscate..
+```lua
+local n = {'a', 'b', 'c'};
+local function f()
+  local r = '';
+  for i=1, #n do
+    r = r .. n[math.random(1, #n)];
+  end
+  return r;
+end
+print(f());
+```
+
+Start obfuscating by running `/obfuscate <key>`.
+
+The bot will prompt you a display with buttons that will help you setup your obfuscation environment:
+
+```bash
+npm init docusaurus@latest my-website classic
+```
+
+**Remember** your **key is valid for 12 hours**, and you **can not obfuscate more scripts at once**.
---
+++ docusaurus.config.js
@@ -0,0 +1,128 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'K4Obfuscator',
+  tagline: 'Free and in continuous development',
+  url: 'https://k4scripts.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'k4scripts', // Usually your GitHub org/user name.
+  projectName: 'k4obfuscator', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark',
+      },
+      navbar: {
+        title: 'K4Obfuscator',
+        logo: {
+          alt: 'K4Scripts Logo',
+          src: 'img/k4scripts.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/k4scripts',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://k4scripts.xyz/discord',
+              }
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/k4scripts',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} K4Scripts, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['lua'],
+      },
+    }),
+};
+
+module.exports = config;
