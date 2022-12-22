469f6206d16a7aedf675fee16b754c545d200022
uniquadev <uniquadev@gmail.com>
2022-12-22 19:54:54 +0100

    local search bar + features

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,44 +2,46 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+import freedom from '@site/static/img/freedom.png';
+import fast from '@site/static/img/fast.png';
+import security from '@site/static/img/security.png';
+
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Free To Go',
+    Png: freedom,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        K4Obfuscator is designed to be free and without limitations. We want to provide the best experience for our users.
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Png: security,
     description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+      <div>
+        K4Obfuscator is frequently updated to maintain a decent obfuscation level meant to be used for simple obfuscation purposes.
+        <br></br><code>Rely on premium obfuscators for more secure obfuscation.</code>
+      </div>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Fast Execution',
+    Png: fast,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        K4Obfuscator vm architecture allow fast code execution which is likely faster than the original code.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Png, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img className={styles.featureSvg} src={Png}/>
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
